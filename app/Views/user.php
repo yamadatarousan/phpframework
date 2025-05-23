dc683b3a14c9278364c9379b9944e7be54dfe22d
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-05-23 23:14:38 +0900

    UserViewを変更

--- app/Views/user.php
+++ app/Views/user.php
@@ -4,6 +4,11 @@
     <title>User</title>
 </head>
 <body>
-    <h1>User ID: <?php echo htmlspecialchars($id); ?></h1>
+    <h1>User ID: <?php echo htmlspecialchars($id ?? 'Unknown'); ?></h1>
+    <?php if ($user): ?>
+        <p>Name: <?php echo htmlspecialchars($user['name'] ?? 'Unknown'); ?></p>
+    <?php else: ?>
+        <p>User not found</p>
+    <?php endif; ?>
 </body>
 </html>
